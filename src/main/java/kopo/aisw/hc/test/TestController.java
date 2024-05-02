802434f4688bd83734a010a63d6205af4cd4f634
DQ <2303110264@office.kopo.ac.kr>
2024-05-02 16:23:19 +0900

    Update lombok file -'click-me-and-restart(lombok x.x.x)'

--- src/main/java/kopo/aisw/hc/test/TestController.java
+++ src/main/java/kopo/aisw/hc/test/TestController.java
@@ -1,12 +1,25 @@
 package kopo.aisw.hc.test;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
 
 @Controller
+@Getter
+@ToString
+@Log4j2
 public class TestController {
-	@RequestMapping("/test")
-	public String testIndex() {
+	
+	@RequestMapping(value="/test", method =RequestMethod.GET)
+	public String testIndex(Locale locale, Model model) {
+		//log.info("Log4j2 Testing...");
 		return "main";
 	}
 //	@RequestMapping("/ob")
