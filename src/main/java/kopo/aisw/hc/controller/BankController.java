9a6f2ed567b0f721dc39e38dfbb85ffd5a2c0bb5
DQ <2303110264@office.kopo.ac.kr>
2024-06-07 22:22:58 +0900

    메인화면 변경

--- src/main/java/kopo/aisw/hc/controller/BankController.java
+++ src/main/java/kopo/aisw/hc/controller/BankController.java
@@ -7,31 +7,30 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Controller
-@RequestMapping("/")
 public class BankController {
-	@RequestMapping("")
+	@RequestMapping("/")
 	public String bankMain() {
 		log.info("Bank Start (L)");
 		return "bankMain";
 	}
 	
-	@RequestMapping("about")
+	@RequestMapping("/about")
 	public String about(){
 		return "example/about";
 	}
-	@RequestMapping("work")
+	@RequestMapping("/work")
 	public String work(){
 		return "example/work";
 	}
-	@RequestMapping("work-single")
+	@RequestMapping("/work-single")
 	public String workSingle(){
 		return "example/work-single";
 	}
-	@RequestMapping("pricing")
+	@RequestMapping("/pricing")
 	public String pricing(){
 		return "example/pricing";
 	}
-	@RequestMapping("contact")
+	@RequestMapping("/contact")
 	public String contact(){
 		return "example/contact";
 	}
